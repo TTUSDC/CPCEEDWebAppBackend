0862c6947baa64ea02dfc4fb55d5ca2e1eac65c8
forlornborg <zachlefevre@gmail.com>
2017-03-01 19:55:44 -0600

    basic routing handled. basic routing methods created

--- Events/router.js
+++ Events/router.js
@@ -1,4 +1,30 @@
 var express = require('express');
+var eventService = require('./services.js');
 var eventRouter = express.Router();
+
+//Create Event
+eventRouter.post('/', function (req, res){
+  var response = eventService.createEvent(req.body);
+  //TODO: send res
+  //TODO: send status
+});
+
+eventRouter.get('/:eventuid', function (req, res){
+  var response = eventService.getEventByID(req.params);
+  //TODO: send res
+})
+
+eventRouter.delete('/:eventuid', function (req, res){
+  var response = eventService.deleteEvent(req.params);
+  //TODO: send res
+  //TODO: send status
+})
+
+eventRouter.get('/', function (req, res){
+  var response = eventService.getAllEvents()
+  //TODO: remove res.send
+  res.send("get all events");
+  //TODO: send res
+})
 
 module.exports = { eventRouter };
---
+++ Events/services.js
@@ -0,0 +1,28 @@
+var response = require('../Objects/response.js');
+
+var createEvent = function (req){
+  //return json containing eventuid
+
+  //TODO: write method and remove console.log
+  console.log(req);
+}
+
+var getEventByID = function (req){
+  //returns json containing an event
+  //TODO: write method and remove console.log
+  console.log(req);
+}
+
+var deleteEvent = function(req){
+  //deletes an event
+  //TODO: write method and remove console.log
+  console.log(req);
+}
+
+var getAllEvents = function(){
+  //TODO: write method and remove console.log
+  console.log("getAllEvents");
+}
+
+
+module.exports = { createEvent, getEventByID, deleteEvent, getAllEvents };
