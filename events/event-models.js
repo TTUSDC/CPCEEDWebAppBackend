cb4d32059d1bdc490bc80eb482cb6776907bfd9a
Zachary LeFevre <zachlefevre@gmail.com>
2017-03-05 15:27:42 -0600

    style changes

--- events/event-models.js
+++ events/event-models.js
@@ -2,15 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var eventSchema = new Schema({
-  category : String,
+  category : {type: String, enum: ['Career','Community','firstother','firstworkshop','mentor','other','outreath','professor','staff','misc']},
   contact : String,
   creator : String,
-  datetime : String,
+  datetime : Date,
   description : String,
   location : String,
   title : String
 });
 
-var Event = mongoose.model('Event', eventSchema, 'events');
+var Event = mongoose.model('Event', eventSchema);
 
 module.exports = { eventModel };
